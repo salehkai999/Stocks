97e3f6d49e7fb6578ca81a2ed033275f6ac92851
saleh <saleh.mk96@gmail.com>
2020-10-22 20:32:16 -0500

    Added saveJSON and loadJSON methods

--- app/src/main/java/com/saleh/stocks/MainActivity.java
+++ app/src/main/java/com/saleh/stocks/MainActivity.java
@@ -5,13 +5,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.JsonWriter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
@@ -21,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RecyclerView recyclerView;
     private StocksAdapter stocksAdapter;
     private SwipeRefreshLayout swiper;
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +45,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         stocksAdapter = new StocksAdapter(stocksList,this);
         recyclerView.setAdapter(stocksAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        for(int i=0;i<5;i++) {
+        loadJSONFile();
+         /*for(int i=0;i<5;i++) {
             stocksList.add(new Stocks("ABC"+i,"Name"+i,0,0,0));
-        }
+        }*/
         stocksAdapter.notifyDataSetChanged();
         swiper.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -73,4 +86,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toast.makeText(this, "OnLongClick", Toast.LENGTH_LONG).show();
         return true;
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveJSON();
+    }
+
+    private void saveJSON() {
+        if(!stocksList.isEmpty()) {
+            try {
+                FileOutputStream fileOutputStream = getApplicationContext().openFileOutput(getString(R.string.file_name), Context.MODE_PRIVATE);
+                JsonWriter jsonWriter = new JsonWriter(new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8));
+                jsonWriter.beginArray();
+                for (Stocks stock : stocksList) {
+                    jsonWriter.setIndent("  ");
+                    jsonWriter.beginObject();
+                    jsonWriter.name("symbol").value(stock.getSymbol());
+                    jsonWriter.name("companyName").value(stock.getCompanyName());
+                    jsonWriter.name("latestPrice").value(stock.getPrice());
+                    jsonWriter.name("change").value(stock.getChange());
+                    jsonWriter.name("changePercent").value(stock.getChangePercent());
+                    jsonWriter.endObject();
+                }
+                jsonWriter.endArray();
+                jsonWriter.close();
+                Log.d(TAG, "saveJSON: " + stocksList.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void loadJSONFile(){
+        try {
+            InputStream inputStream = getApplicationContext().openFileInput(getString(R.string.file_name));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                Log.d(TAG, "loadJSONDate: " + line);
+            }
+            JSONArray jsonArray = new JSONArray(builder.toString());
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                Stocks stocks = new Stocks();
+                stocks.setSymbol(jsonObject.getString("symbol"));
+                stocks.setCompanyName(jsonObject.getString("companyName"));
+                stocks.setPrice(jsonObject.getDouble("latestPrice"));
+                stocks.setChange(jsonObject.getDouble("change"));
+                stocks.setChangePercent(jsonObject.getDouble("changePercent"));
+                stocksList.add(stocks);
+            }
+            Collections.sort(stocksList);
+            stocksAdapter.notifyDataSetChanged();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, "NO FILE", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
