78da2631fc03f9737e5d4b89c7ac3f76d505a92e
saleh <saleh.mk96@gmail.com>
2020-10-22 21:41:44 -0500

    Added SymbolNameDownloader and EditText dialog.

--- app/src/main/java/com/saleh/stocks/MainActivity.java
+++ app/src/main/java/com/saleh/stocks/MainActivity.java
@@ -1,18 +1,27 @@
 package com.saleh.stocks;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.JsonWriter;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.saleh.stocks.downloaders.SymbolNameDownloader;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -35,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private StocksAdapter stocksAdapter;
     private SwipeRefreshLayout swiper;
     private static final String TAG = "MainActivity";
+    private static String stocksURL = "https://www.marketwatch.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +55,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         stocksAdapter = new StocksAdapter(stocksList,this);
         recyclerView.setAdapter(stocksAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        SymbolNameDownloader symbolNameDownloader = new SymbolNameDownloader();
+        new Thread(symbolNameDownloader).start();
+
         loadJSONFile();
          /*for(int i=0;i<5;i++) {
             stocksList.add(new Stocks("ABC"+i,"Name"+i,0,0,0));
@@ -70,6 +83,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.add) {
             Toast.makeText(this, "ADD", Toast.LENGTH_SHORT).show();
+            choiceDialog();
+            //stocksList.add(new Stocks("TGT","Target",0.0,0.0,0.0));
+            //stocksAdapter.notifyDataSetChanged();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -78,7 +94,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Toast.makeText(this, "OnClick", Toast.LENGTH_SHORT).show();
-
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(stocksURL));
+        startActivity(intent);
     }
 
     @Override
@@ -148,6 +166,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             e.printStackTrace();
             Toast.makeText(this, "NO FILE", Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    private void choiceDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText stockText = new EditText(this);
+        stockText.setInputType(InputType.TYPE_CLASS_TEXT);
+        stockText.setGravity(Gravity.CENTER_HORIZONTAL);
+        //stockText.
+        builder.setView(stockText);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String text = stockText.getText().toString().trim();
+                Log.d(TAG, "onClick: "+text);
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+                listDialog(text);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "CANCELED", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setMessage("Enter Symbol or Name");
+        builder.setTitle("Select Stock");
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    private void listDialog(String choice){
 
     }
 
---
+++ app/src/main/java/com/saleh/stocks/downloaders/SymbolNameDownloader.java
@@ -0,0 +1,101 @@
+package com.saleh.stocks.downloaders;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class SymbolNameDownloader implements Runnable {
+
+    private static final String TAG = "SymbolNameDownloader";
+    private static final String STOCKS_SYMBOL_URL = "https://api.iextrading.com/1.0/ref-data/symbols";
+    public static final HashMap<String,String> symbolNameMap = new HashMap<>();
+
+
+    @Override
+    public void run() {
+        Uri stocksUri = Uri.parse(STOCKS_SYMBOL_URL);
+        String urlString = stocksUri.toString();
+        Log.d(TAG, "run: "+urlString);
+        StringBuilder dataBuilder = new StringBuilder();
+
+        try{
+            URL url = new URL(urlString);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                Log.d(TAG, "run: HTTP ResponseCode NOT OK: " + conn.getResponseCode());
+                return;
+            }
+
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader((new InputStreamReader(is)));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                dataBuilder.append(line).append('\n');
+            }
+
+            Log.d(TAG, "run: " + dataBuilder.toString());
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        processData(dataBuilder.toString());
+
+    }
+
+    private void processData(String data) {
+        try {
+            JSONArray jsonArray = new JSONArray(data);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                String symbol = jsonObject.getString("symbol");
+                String name = jsonObject.getString("name");
+                Log.d(TAG, "processData: "+symbol+" : "+name);
+                symbolNameMap.put(symbol, name);
+            }
+            Log.d(TAG, "process: ");
+        } catch (Exception e) {
+            Log.d(TAG, "parseJSON: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static List<String> getMatches(String match){
+        String matcher = match.toLowerCase().trim();
+        HashSet<String> matchSet = new HashSet<>();
+        for(String symbol : symbolNameMap.keySet()) {
+            if(symbol.trim().toLowerCase().contains(matcher)) {
+                matchSet.add(symbol+" - "+symbolNameMap.get(symbol));
+            }
+            String name = symbolNameMap.get(symbol);
+            if (name != null &&
+                    name.toLowerCase().trim().contains(matcher)) {
+                matchSet.add(symbol + " - " + name);
+            }
+        }
+        ArrayList<String> matched = new ArrayList<>(matchSet);
+        Collections.sort(matched);
+        return matched;
+    }
+
+}
